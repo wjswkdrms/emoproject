283ef09bb001b9f81e4f0be820df4eef424c9db6
EESAMSAOH <hoyo1599@gmail.com>
2023-06-19 15:48:35 +0900

    수정

--- emoproject/src/main/webapp/sql/eesamsaoh.sql
+++ emoproject/src/main/webapp/sql/eesamsaoh.sql
@@ -1,7 +1,7 @@
 --공지사항
 CREATE TABLE em_board_announce(
  ann_num number, --공지사항 번호
- mem_num number, --회원 번호
+ mem_num number not null, --회원 번호
  ann_title varchar2(300) not null, --공지사항 제목
  ann_content varchar2(1000) not null, --공지사항 내용
  ann_photo1 varchar2(100), --게시판 사진
@@ -15,7 +15,7 @@ CREATE SEQUENCE em_board_announce_seq;
 --1:1 문의 게시판
 CREATE TABLE em_board_ask(
  ask_num number,--문의글 번호
- mem_num number,--문의자 번호
+ mem_num number not null,--문의자 번호
  ask_title varchar2(100) not null,--문의글 제목
  ask_content varchar2(1000) not null,--문의글 내용
  ask_photo1 varchar2(100),--문의글 사진 첨부
@@ -29,8 +29,8 @@ CREATE SEQUENCE em_board_ask_seq;
 
 --1:1 문의 답변
 CREATE TABLE em_board_answer(
- mem_num number,--관리자 번호
- ask_num number,--문의글 번호
+ mem_num number not null,--관리자 번호
+ ask_num number not null,--문의글 번호
  answer_num number,
  answer_content varchar2(1000) not null,--답변 내용
  answer_photo varchar2(100),--답변 사진 첨부
@@ -45,8 +45,8 @@ CREATE SEQUENCE em_board_answer_seq;
 --제품 후기
 CREATE TABLE em_review(
  review_num number,--리뷰 글 번호
- product_num number,--상품 번호
- mem_num number,--회원 번호
+ product_num number not null,--상품 번호
+ mem_num number not null,--회원 번호
  review_title varchar2(100) not null,--리뷰 제목
  review_content varchar2(1000) not null,--리뷰 내용
  review_photo1 varchar2(100),--리뷰 사진
