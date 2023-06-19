53f8d68e5fab61af6a023e590fa916cf21dac9f2
user <user@DESKTOP-COJ9858>
2023-06-19 15:29:05 +0900

    table 수정

---
+++ emoproject/src/main/java/kr/cart/dao/CartDAO.java
@@ -0,0 +1,5 @@
+package kr.cart.dao;
+
+public class CartDAO {
+
+}
---
+++ emoproject/src/main/java/kr/cart/vo/CartVO.java
@@ -0,0 +1,35 @@
+package kr.cart.vo;
+
+public class CartVO {
+	private int cart_num;//장바구니 번호
+	private int mem_num;//회원번호
+	private int product_num;//상품번호
+	private int cart_quantity;//구매 상품 수량
+	
+	public int getCart_num() {
+		return cart_num;
+	}
+	public void setCart_num(int cart_num) {
+		this.cart_num = cart_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public int getProduct_num() {
+		return product_num;
+	}
+	public void setProduct_num(int product_num) {
+		this.product_num = product_num;
+	}
+	public int getCart_quantity() {
+		return cart_quantity;
+	}
+	public void setCart_quantity(int cart_quantity) {
+		this.cart_quantity = cart_quantity;
+	}
+	
+	
+}
